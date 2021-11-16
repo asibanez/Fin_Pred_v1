830ce258634798407e42b9f462f48c3d9ad9f25b
asibanez <sibanez@sloan.mit.edu>
2021-11-16 00:42:20 +0000

    run update

--- 03_models/00_binary/02_full_dataset/run.sh
+++ 03_models/00_binary/02_full_dataset/run.sh
@@ -1,13 +1,10 @@
 #INPUT_DIR=C:/Users/siban/Dropbox/BICTOP/MyInvestor/06_model/02_NLP/03_spy_project/00_data/02_preprocessed/02_ProsusAI_finbert/00_binary/01_full_dataset
-INPUT_DIR=C:/Users/siban/Dropbox/BICTOP/MyInvestor/06_model/02_NLP/03_spy_project/00_data/02_preprocessed/02_ProsusAI_finbert/00_binary/02_full_TEST_DELETE
+#OUTPUT_DIR=C:/Users/siban/Dropbox/BICTOP/MyInvestor/06_model/02_NLP/03_spy_project/00_data/03_runs/01_ProsusAI_finbert/01_run_binary_full
 
+INPUT_DIR=/home/sibanez/Projects/MyInvestor/NLP/01_spyproject/00_data/02_preprocessed/02_ProsusAI_finbert/00_binary/01_FULL
+OUTPUT_DIR=/home/sibanez/Projects/MyInvestor/NLP/01_spyproject/02_runs/01_TEST_1
 
-OUTPUT_DIR=C:/Users/siban/Dropbox/BICTOP/MyInvestor/06_model/02_NLP/03_spy_project/00_data/03_runs/01_ProsusAI_finbert/01_run_binary_full
-
-#INPUT_DIR=/data/rsg/nlp/sibanez/00_temp/01_fin_pred/00_data/02_preprocessed/02_ProsusAI_finbert/00_binary
-#OUTPUT_DIR=/data/rsg/nlp/sibanez/00_temp/01_fin_pred/00_data/03_runs/01_ProsusAI_finbert/00_run_binary
-
-python -m ipdb train_test.py \
+python train_test.py \
     --input_dir=$INPUT_DIR \
     --output_dir=$OUTPUT_DIR \
     --task=Train \
@@ -22,7 +19,7 @@ python -m ipdb train_test.py \
     --use_cuda=True \
     \
     --n_epochs=10 \
-    --batch_size_train=400 \
+    --batch_size_train=1600 \
     --shuffle_train=True \
     --drop_last_train=False \
     --dev_train_ratio=2 \
@@ -35,7 +32,7 @@ python -m ipdb train_test.py \
     --save_final_model=True \
     --save_model_steps=True \
     --save_step_cliff=0 \
-    --gpu_ids_train=0 \
+    --gpu_ids_train=0,1,2,3 \
     \
     --test_file=model_test.pkl \
     --model_file=model.pt.3 \
