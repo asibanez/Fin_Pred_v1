2f213f3a2b7a289aeacb61499a08e24c171b75f4
asibanez <sibanez@sloan.mit.edu>
2022-01-12 22:08:47 +0000

    ops update

--- 03_models/00_binary/05_BOW_dataset_MA/run.sh
+++ 03_models/00_binary/05_BOW_dataset_MA/run.sh
@@ -1,8 +1,8 @@
 #INPUT_DIR=C:/Users/siban/Dropbox/BICTOP/MyInvestor/06_model/02_NLP/03_spy_project/00_data/02_preprocessed/04_BOW/00_binary/00_2018
 #OUTPUT_DIR=C:/Users/siban/Dropbox/BICTOP/MyInvestor/06_model/02_NLP/03_spy_project/00_data/03_runs/03_BOW/00_TEST
 
-INPUT_DIR=/home/sibanez/Projects/00_MyInvestor/00_data/02_preprocessed/04_BOW/00_binary/00_TEST
-OUTPUT_DIR=/home/sibanez/Projects/00_MyInvestor/00_data/03_runs/03_BOW/00_TEST
+INPUT_DIR=/home/sibanez/Projects/00_MyInvestor/00_data/02_preprocessed/04_BOW/00_binary/01_ALL
+OUTPUT_DIR=/home/sibanez/Projects/00_MyInvestor/00_data/03_runs/03_BOW/02_FULL_non_empty_25ep
 
 python train_test.py \
     --input_dir=$INPUT_DIR \
@@ -15,7 +15,7 @@ python train_test.py \
     --use_cuda=True \
     --remove_empty_entries=True \
     \
-    --n_epochs=200 \
+    --n_epochs=25 \
     --batch_size_train=6000 \
     --shuffle_train=True \
     --drop_last_train=False \
@@ -28,11 +28,11 @@ python train_test.py \
     --momentum=0.9 \
     --save_final_model=True \
     --save_model_steps=True \
-    --save_step_cliff=0 \
+    --save_step_cliff=15 \
     --gpu_ids_train=0 \
     \
     --test_file=model_test.pkl \
-    --model_file=model.pt.50 \
+    --model_file=model.pt.20 \
     --batch_size_test=200 \
     --gpu_id_test=0 \
 
